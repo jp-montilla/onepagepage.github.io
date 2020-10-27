cbb7d1d29c5ca3b81ffddeb7b41606215f37da8a
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2020-10-28 02:28:38 +0800

    1st section

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+         custom_green: '#0fcc5b',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
